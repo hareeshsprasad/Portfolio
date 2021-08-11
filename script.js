ec04a616d63afcdc3a1f97286197d9cba2e6c5f0
hareeshsprasad <hareeshsprasad@gmail.com>
2021-08-11 17:30:03 +0530

    portfolio

--- script.js
+++ script.js
@@ -41,12 +41,12 @@ $(document).ready(function(){
     //     loop: true
     // });
 
-    var typed = new Typed(".typing-2", {
-        strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
-    });
+    // var typed = new Typed(".typing-3", {
+    //     strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
+    //     typeSpeed: 100,
+    //     backSpeed: 60,
+    //     loop: true
+    // });
 
     // owl carousel script
     $('.carousel').owlCarousel({
